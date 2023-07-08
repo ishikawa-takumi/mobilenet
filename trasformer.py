ce786010c98708579f7f9c68eaf1295316f066a9
takumi-ishikawa <t-ishikawa@think-lands.com>
2023-07-08 21:35:09 +0900

    add components for trasfer learning

---
+++ trasformer.py
@@ -0,0 +1,22 @@
+from torchvision import transforms
+
+class ImageTransform():
+    def __init__(self, resize, mean, std):
+        self.data_transform = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(
+                    resize, scale=(0.5, 1.0)),  # データオーギュメンテーション
+                transforms.RandomHorizontalFlip(),  # データオーギュメンテーション
+                transforms.ToTensor(),  # テンソルに変換
+                transforms.Normalize(mean, std)  # 標準化
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(resize),  # リサイズ
+                transforms.CenterCrop(resize),  # 画像中央をresize×resizeで切り取り
+                transforms.ToTensor(),  # テンソルに変換
+                transforms.Normalize(mean, std)  # 標準化
+            ])
+        }
+
+    def __call__(self, img, phase='train'):
+        return self.data_transform[phase](img)
